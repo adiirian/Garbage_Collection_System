14ef2f9eb5d29ffa3f8bf46caab55bb779edb081
Chad <chapamd18gmail.com>
2025-10-23 10:10:35 +0800

    Updated Default Database Structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(RoleSeeder::class);
+        $this->call(BinSeeder::class);
+        $this->call(UserSeeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Create alerts for bins that are not empty
+        $bins = \App\Models\Bin::where('level', '!=', 'empty')->get();
+        foreach ($bins as $bin) {
+            \App\Models\Alert::firstOrCreate(
+                [
+                    'bin_id' => $bin->id,
+                    'type' => 'level_change',
+                    'level' => $bin->level,
+                ],
+                [
+                    'message' => "Bin {$bin->name} is {$bin->level}",
+                    'status' => 'open',
+                ]
+            );
+        }
     }
 }
